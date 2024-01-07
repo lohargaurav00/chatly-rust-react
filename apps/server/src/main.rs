06bcfdf31ae7a12d2dcda277b6587f142630f1f7
lohargaurav00 <lohargaurav00@gmail.com>
2024-01-07 15:03:01 +0530

    socketio server

--- apps/server/src/main.rs
+++ apps/server/src/main.rs
@@ -1,4 +1,41 @@
-fn main() {
-    println!("Hello, world!");
-}
+use axum::{
+    routing::get,
+    Router,
+};
+use tokio::net::TcpListener;
+use dotenv::dotenv;
+use std::env;
+use socketioxide::{
+    extract::{AckSender, SocketRef, Bin, Data},
+    SocketIo,
+};
+use serde_json::Value;
+use tracing::info;
+// use tracing_subscriber::FmtSubscriber;
 
+
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn std::error::Error>> {
+
+    // tracing::subscriber::set_global_default(FmtSubscriber::default())?;
+    dotenv().ok();
+
+    let port = env::var("PORT").unwrap_or_else(|_| "8000".to_string());
+    let url = format!("0.0.0.0:{}", port);
+
+    let (layer, io) = SocketIo::new_layer();
+
+    io.ns("/", |socket: SocketRef, Data(data): Data<Value>|{
+        info!("Socket.IO connected: {:?} {:?}", socket.ns(), socket.id);
+        socket.emit("msg", "Hello from socket");
+    });
+
+    let app = Router::new()
+        .route("/", get(|| async { "Hello, World!" }))
+        .layer(layer);
+
+    let lister = TcpListener::bind(url).await?;
+    axum::serve(lister, app).await?;
+
+    Ok(())
+}
