7f379e1e2d8773bededa3485e98d73df45656130
lohargaurav00 <lohargaurav00@gmail.com>
2024-07-21 19:14:25 +0530

    chore: Update actix-web and serde dependencies

--- scripts/dev.sh
+++ scripts/dev.sh
@@ -8,10 +8,23 @@ start_frontend() {
     echo "Frontend Started with PID $FRONTEND_PID"
 }
 
+# Function to start the backend with cargo watch
+start_backend_with_watch() {
+    echo "Starting Backend with cargo watch"
+    (cd server && cargo watch -x run)
+}
+
+# Function to start the backend with cargo run as a fallback
+start_backend_fallback() {
+    echo "cargo watch failed, falling back to cargo run"
+    (cd server && cargo run)
+}
+
+
 # Function to start the backend
 start_backend() {
     echo "Starting Backend"
-    (cd server && cargo run) &  # Start the backend process in the background
+    start_backend_with_watch || start_backend_fallback &  # Start the backend process in the background
     BACKEND_PID=$!
     echo "Backend Started with PID $BACKEND_PID"
 }
--- server/src/main.rs
+++ server/src/main.rs
@@ -1,3 +1,14 @@
-fn main() {
-    println!("Hello, world!");
+use actix_web::{get, App, HttpResponse, HttpServer, Responder};
+
+#[get("/")]
+async fn greet() -> impl Responder {
+    HttpResponse::Ok().body("Hello World")
+}
+
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
+    HttpServer::new(|| App::new().service(greet))
+        .bind(("127.0.0.1", 8000))?
+        .run()
+        .await
 }
