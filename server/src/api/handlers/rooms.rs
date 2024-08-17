9059a6d4a17e59482bfcea590ab761b143d49090
lohargaurav00 <lohargaurav00@gmail.com>
2024-08-17 19:16:21 +0530

    refactor: Add join-room and get-room-with-members API routes and handlers

--- server/src/api/handlers/rooms.rs
+++ server/src/api/handlers/rooms.rs
@@ -2,7 +2,7 @@ use actix_web::{get, http::StatusCode, post, web, HttpResponse};
 use sqlx::PgPool;
 
 use crate::{
-    models::{NewRoom, Room},
+    models::{JoinRoom, NewRoom, Room, RoomWithMembers},
     utils::helpers::{handle_response, Status},
 };
 
@@ -87,8 +87,111 @@ pub async fn get_room_by_id(db_pool: web::Data<PgPool>, id: web::Path<i64>) -> H
     }
 }
 
+#[post("join-room")]
+pub async fn join_room(db_pool: web::Data<PgPool>, join_room: web::Json<JoinRoom>) -> HttpResponse {
+    let join_room = JoinRoom {
+        id: join_room.id.clone(),
+        room_id: join_room.room_id.clone(),
+    };
+
+    let join_result = sqlx::query_as!(
+        RoomWithMembers,
+        r#"
+WITH inserted AS (
+    INSERT INTO room_users (room_id, user_id)
+    VALUES ($1, $2)
+    RETURNING room_id, user_id
+)
+SELECT 
+    r.id, 
+    r.name, 
+    r.last_message, 
+    r.created_by, 
+    r.created_at, 
+    r.updated_at, 
+    COALESCE(
+        json_agg(row_to_json(u)),
+        '[]'
+    ) AS members
+FROM rooms r
+LEFT JOIN (
+    SELECT ru.room_id, u.*
+    FROM room_users ru
+    JOIN users u ON u.id = ru.user_id
+    WHERE ru.room_id IN (SELECT room_id FROM inserted)
+    UNION ALL
+    SELECT i.room_id, u.*
+    FROM inserted i
+    JOIN users u ON u.id = i.user_id
+) u ON u.room_id = r.id
+WHERE r.id IN (SELECT room_id FROM inserted)
+GROUP BY r.id;
+
+"#,
+        join_room.room_id,
+        join_room.id
+    )
+    .fetch_all(db_pool.get_ref())
+    .await;
+
+    match join_result {
+        Ok(room) => handle_response(
+            StatusCode::OK,
+            Status::Ok,
+            "Room joined successfully",
+            Some(room),
+        ),
+        Err(e) => handle_response(
+            StatusCode::INTERNAL_SERVER_ERROR,
+            Status::Error,
+            &format!("Failed to join room: {:?}", e),
+            Some(()),
+        ),
+    }
+}
+
+#[get("room-with-members/{id}")]
+pub async fn get_room_with_members(db_pool: web::Data<PgPool>, id: web::Path<i32>) -> HttpResponse {
+    let room_result = sqlx::query_as!(
+        RoomWithMembers,
+        r#"
+    SELECT 
+    r.*,
+    COALESCE(
+        json_agg(row_to_json(u)),
+        '[]'
+    ) AS members
+FROM rooms r
+LEFT JOIN room_users ru ON ru.room_id = r.id
+LEFT JOIN users u ON u.id = ru.user_id
+WHERE r.id = $1
+GROUP BY r.id;
+"#,
+        id.into_inner()
+    )
+    .fetch_one(db_pool.get_ref())
+    .await;
+
+    match room_result {
+        Ok(room) => handle_response(
+            StatusCode::OK,
+            Status::Ok,
+            "Room fetched successfully",
+            Some(room),
+        ),
+        Err(e) => handle_response(
+            StatusCode::INTERNAL_SERVER_ERROR,
+            Status::Error,
+            &format!("Failed to fetch room: {:?}", e),
+            Some(()),
+        ),
+    }
+}
+
 pub fn init_routes(cfg: &mut web::ServiceConfig) {
     cfg.service(create_room);
     cfg.service(get_rooms);
     cfg.service(get_room_by_id);
+    cfg.service(join_room);
+    cfg.service(get_room_with_members);
 }
--- server/src/models.rs
+++ server/src/models.rs
@@ -1,25 +1,15 @@
 // src/models.rs
 
+use std::collections::HashMap;
+
 use serde::{self, Deserialize, Serialize};
-use sqlx::prelude::FromRow;
+use serde_json::Value;
+use sqlx::{prelude::FromRow, types::Json};
 use uuid::Uuid;
 
-#[derive(Debug, Default, Serialize, Deserialize, Clone, PartialEq, FromRow)]
-pub struct NewRoom {
-    pub name: String,
-    pub created_by: Uuid,
-}
+//#Models for the database tables
 
-#[derive(Debug, Default, Serialize, Deserialize, Clone, PartialEq, FromRow)]
-pub struct Room {
-    pub id: i64,
-    pub name: String,
-    pub last_message: Option<String>,
-    pub created_by: Uuid,
-    pub created_at: i64,
-    pub updated_at: Option<i64>,
-}
-
+// #User model
 #[derive(Debug, Default, Serialize, Deserialize, Clone, PartialEq, FromRow)]
 pub struct User {
     pub id: Uuid,
@@ -31,4 +21,52 @@ pub struct User {
     pub profile_photo: Option<String>,
     pub created_at: i64,
     pub updated_at: Option<i64>,
+}
+
+#[derive(Debug, Default, Serialize, Deserialize, Clone, PartialEq, FromRow)]
+pub struct UserWithRooms {
+    pub id: Uuid,
+    pub name: String,
+    #[serde(rename = "username")]
+    pub user_name: String,
+    pub email: String,
+    pub password: String,
+    pub profile_photo: Option<String>,
+    pub created_at: i64,
+    pub updated_at: Option<i64>,
+    pub rooms: Vec<Room>,
+}
+
+// #Room model
+#[derive(Debug, Default, Serialize, Deserialize, Clone, PartialEq, FromRow)]
+pub struct NewRoom {
+    pub name: String,
+    pub created_by: Uuid,
+}
+
+#[derive(Debug, Default, Serialize, Deserialize, Clone, PartialEq, FromRow)]
+pub struct Room {
+    pub id: i32,
+    pub name: String,
+    pub last_message: Option<String>,
+    pub created_by: Uuid,
+    pub created_at: i64,
+    pub updated_at: Option<i64>,
+}
+
+#[derive(Debug, Default, Serialize, Deserialize, Clone, PartialEq, FromRow)]
+pub struct RoomWithMembers {
+    pub id: i32,
+    pub name: String,
+    pub last_message: Option<String>,
+    pub created_by: Uuid,
+    pub created_at: i64,
+    pub updated_at: Option<i64>,
+    pub members: Option<Value>,
+}
+
+#[derive(Debug, Default, Serialize, Deserialize, Clone, PartialEq, FromRow)]
+pub struct JoinRoom {
+    pub id: Uuid,
+    pub room_id: i32,
 }
