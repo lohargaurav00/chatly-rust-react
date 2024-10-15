86d3dbeaafbea1ac4f292b71b661e8e21af31ea1
lohargaurav00 <lohargaurav00@gmail.com>
2024-10-15 22:51:37 +0530

    prisma schema and some layout fix

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -7,7 +7,7 @@ interface LayoutProps {
 
 const Layout: React.FC<LayoutProps> = ({ children }) => {
   return (
-    <div className="h-screen w-full flex overflow-hidden">
+    <div className="h-full w-full flex overflow-hidden">
       <Sidebar />
       <div className="flex flex-col h-full w-full">
         <Header />
